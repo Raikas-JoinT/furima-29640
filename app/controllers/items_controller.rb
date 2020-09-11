b468d8c50ff8cfad68bcf25062a2b4bba897dacb
Raikas-JoinT <ketukoketuo@gmail.com>
2020-09-12 01:08:51 +0900

    プルダウンの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,29 @@
 class ItemsController < ApplicationController
   def index
     @items = Item.all
+    @items = Article.order("created_at DESC")
+  end
+
+  def new
+    @item = Article.new
+  end
+
+  def edit
+  end
+
+  def create
+    @items = Article.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:title ,:text ,:category_id ,:status_id, :burden_id ,:area_id ,:day_id)
   end
 
   def item_params
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :status
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :area
+  belongs_to_active_hash :day
 
+  validates :title, :text, :category, presence: true
+  validates :title, :text, :status, presence: true
+  validates :title, :text, :burden, presence: true
+  validates :title, :text, :area, presence: true
+  validates :title, :text, :day, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :burden_id, numericality: { other_than: 1 }
+  validates :area_id, numericality: { other_than: 1 }
+  validates :day_id, numericality: { other_than: 1 }
   belongs_to :user
   has_one :addresses_item
   has_one_attached :image
